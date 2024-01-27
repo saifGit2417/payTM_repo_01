ac99c176f227bcc3b76073b56d92dcc20f295ae3
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-01-28 00:37:18 +0530

    get bank balance api

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,16 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth_middleware.js";
+import { AccountDetails } from "../db/index.js";
 
 const accountRouter = express.Router();
+
+accountRouter.get("/balance", authMiddleware, async (req, res) => {
+  const { userId } = req;
+  const fetchBankBalanceByUserId = await AccountDetails.findOne({ userId });
+  res.json({
+    message: "balance fetched successfully",
+    balance: fetchBankBalanceByUserId.balance,
+  });
+});
 
 export default accountRouter;
