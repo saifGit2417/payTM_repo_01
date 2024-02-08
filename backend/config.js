a20fcf63b2ec9855516a61dc2cba23e0cc440623
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-02-08 15:45:08 +0530

    .env config

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const JWT_SECRET_KEY = "100X_payTmApp_JWT_KEY";
-export const DB_URL =
-  "  `mongodb+srv://saif100x:5PVhSqYJ9G9nU3Nb@100xcluster.gdbofct.mongodb.net/payTm_repo`";
+
 export const PORT_NUMBER = 8007;
--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,8 @@
 import mongoose, { Schema } from "mongoose";
-import { DB_URL } from "../config.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const DB_URL = process.env.DB_URL;
 
 mongoose.connect(DB_URL).then(() => console.log("db connected successfully"));
 
