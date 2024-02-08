1465ade7fe63ba33284818f94a142f3d9a794939
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-02-08 16:42:42 +0530

    transaction api enabled

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -14,57 +14,51 @@ accountRouter.get("/balance", authMiddleware, async (req, res) => {
   });
 });
 
-// accountRouter.post("/transfer", authMiddleware, async (req, res) => {
-//   console.log("entered");
-//   const session = await mongoose.startSession();
-//   console.log("entered 1");
+accountRouter.post("/transfer", authMiddleware, async (req, res) => {
+  const session = await mongoose.startSession();
 
-//   session.startTransaction();
-//   const { amount, to } = req.body;
-//   console.log("entered 2");
+  session.startTransaction();
+  const { amount, to } = req.body;
 
-//   // Fetch the accounts within the transaction
-//   const account = await AccountDetails.findOne({ userId: req.userId }).session(
-//     session
-//   );
-//   console.log("entered 3");
+  // Fetch the accounts within the transaction
+  const account = await AccountDetails.findOne({ userId: req.userId }).session(
+    session
+  );
 
-//   if (!account || account.balance < amount) {
-//     await session.abortTransaction();
-//     return res.status(400).json({
-//       message: "Insufficient balance",
-//     });
-//   }
-//   console.log("entered 4");
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
 
-//   const toAccount = await AccountDetails.findOne({ userId: to }).session(
-//     session
-//   );
-//   console.log("entered 5");
+  const toAccount = await AccountDetails.findOne({ userId: to }).session(
+    session
+  );
 
-//   if (!toAccount) {
-//     await session.abortTransaction();
-//     return res.status(400).json({
-//       message: "Invalid account",
-//     });
-//   }
+  if (!toAccount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Invalid account",
+    });
+  }
 
-//   // Perform the transfer
-//   await AccountDetails.updateOne(
-//     { userId: req.userId },
-//     { $inc: { balance: -amount } }
-//   ).session(session);
+  // Perform the transfer
+  await AccountDetails.updateOne(
+    { userId: req.userId },
+    { $inc: { balance: -amount } }
+  ).session(session);
 
-//   await AccountDetails.updateOne(
-//     { userId: to },
-//     { $inc: { balance: amount } }
-//   ).session(session);
+  await AccountDetails.updateOne(
+    { userId: to },
+    { $inc: { balance: amount } }
+  ).session(session);
 
-//   // Commit the transaction
-//   await session.commitTransaction();
-//   res.json({
-//     message: "Transfer successful",
-//   });
-// });
+  // Commit the transaction
+  await session.commitTransaction();
+  res.json({
+    message: "Transfer successful",
+  });
+});
 
 export default accountRouter;
